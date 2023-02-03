3052da20efd8985e4acf2f66f66c477ea4e4d95a
RisPlukkern <duungtr@hotmail.com>
2023-02-03 09:54:38 +0100

    Test again for pull

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) =>{
-    res.send('Graduation Project Start Up!!')
+    res.send('One more time')
 })
 
 app.listen(PORT, () => {
